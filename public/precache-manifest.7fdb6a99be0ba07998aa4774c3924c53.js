a38619e045eab2a644705003c77bb81a793fd4f5
yuanfei2 <yuanfei2.illinois.edu>
2023-03-24 01:16:43 -0500

    changed starting script

---
+++ public/precache-manifest.7fdb6a99be0ba07998aa4774c3924c53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3fc1e1c6c791103861ec27616a4f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf4142f043a1482018d8",
+    "url": "/static/css/main.e6c5e19d.chunk.css"
+  },
+  {
+    "revision": "8971acce2be7ced93f70",
+    "url": "/static/js/2.2750d2f8.chunk.js"
+  },
+  {
+    "revision": "cf4142f043a1482018d8",
+    "url": "/static/js/main.6735de97.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
